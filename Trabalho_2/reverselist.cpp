693bd7110591b61f405892c2b88c5fc4c4094cb5
henrique-mariano <henrique_mariano@aluno.unb.br>
2019-05-08 20:37:08 -0300

    Fazendo testes com vector

--- Trabalho_2/reverselist.cpp
+++ Trabalho_2/reverselist.cpp
@@ -1,24 +1,36 @@
 #include <iostream>
 #include <list>
+#include <vector>
 
+#define N 10e2 + 1
 using namespace std;
 
 void reverse_list(list<int>&);
 void swap_list(list<int>&, size_t i, size_t j);
 void print_list(list<int>&);
+void reverse_vector(vector<int>&);
+void print_vector(vector<int>&);
 
 int main(void) {
-    list<int> lista = {1,2,3,4,5};
+    list<int> lista;
+    //vector<int> vec(N);
+    for(size_t k = 1; k < N; k++) {
+        //vec.push_back(k);
+        lista.push_back(k);
+    }
     cout << "Lista antes do reverse:" << endl;
     print_list(lista);
     reverse_list(lista);
+    //print_vector(vec);
+    //reverse_vector(vec);
     cout << "Lista apos o reverse:" << endl;
+    //print_vector(vec);
     print_list(lista);
     return 0;
 }
 
 void reverse_list(list<int>& lista) {
-    for(size_t i = 0, j = lista.size() - 1; i < j; i++, j--){
+    for(size_t i = 0, j = lista.size() - 1; i < j; i++, j--) {
         swap_list(lista, i, j);
     }
     return;
@@ -39,10 +51,26 @@ void swap_list(list<int>& lista, size_t i, size_t j) {
     return;
 }
 
+void reverse_vector(vector<int>& vec) {
+    for(size_t i = 0, j = vec.size() - 1; i < j; i++, j--) {
+        vec[i] += vec[j];
+        vec[j] = vec[i] - vec[j];
+        vec[i] -= vec[j];
+    }
+}
+
 void print_list(list<int>& lista) {
     cout << "Lista:";
-    for(auto i = lista.begin(); i != lista.end(); ++i){
-        cout << " " << *i;
+    for(auto i = lista.begin(); i != lista.end(); ++i) {
+        printf(" %d", *i);
+    }
+    cout << endl;
+}
+
+void print_vector(vector<int>& vec) {
+    cout << "Vector:";
+    for(auto i = vec.begin(); i != vec.end(); ++i) {
+        printf(" %d", *i);
     }
     cout << endl;
 }
