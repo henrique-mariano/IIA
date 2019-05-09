b1bbe176b27e1164d7967d211038a02a30e4ecd0
henrique-mariano <henrique_mariano@aluno.unb.br>
2019-05-09 15:57:33 -0300

    Algoritmos de inversão em prolog

---
+++ Trabalho_2/reverselist.pl
@@ -0,0 +1,12 @@
+%Concatenação
+conc([], L, L).
+conc([X|L1], L2, [X|L3]) :- conc(L1,L2,L3).
+
+%Inversão O(n^2)
+invertern2([],[]).
+invertern2([X|Y], Z) :- invertern2(Y, Y1), conc(Y1,[X],Z).
+
+%Inversão O(N)
+invertern(X, Y) :- aux([], X, Y).
+aux(L,[],L).
+aux(L,[X|Y], Z) :- aux([X|L], Y, Z).
